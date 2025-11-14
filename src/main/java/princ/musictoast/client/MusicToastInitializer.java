d195cfcd425159aa8b8571e1a4faa04926aac8e4
Touhidur³ <164875986+PrincParshia@users.noreply.github.com>
2025-11-14 23:22:15 +0600

    Delete src directory

--- src/main/java/princ/musictoast/client/MusicToastInitializer.java
+++
@@ -1,8 +0,0 @@
-package princ.musictoast.client;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class MusicToastInitializer implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {}
-}
